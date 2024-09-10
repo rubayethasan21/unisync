26650ad575158b0b78b9ab2248244475b209200c
Tahir Siddique <tahirsiddiquework@gmail.com>
2024-09-10 11:01:29 +0500

    Added host

--- fixed_app.py
+++ fixed_app.py
@@ -219,4 +219,4 @@ def screenshot(session_id):
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
