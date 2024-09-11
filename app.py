efd7b22d609592ec024e57ffdca50ac78dd74494
rubayet21itis <rubayet21itis@gmail.com>
2024-09-11 18:18:09 +0200

    for server prepare

--- app.py
+++ app.py
@@ -235,4 +235,5 @@ async def cleanup_session(session_id):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5001)
+    #uvicorn.run(app, host="0.0.0.0", port=5001)
+    uvicorn.run(app)
